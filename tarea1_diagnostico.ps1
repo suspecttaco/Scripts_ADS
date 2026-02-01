3aed2ccfdb673815959fcf711ebf03eb604b5e68
suspecttaco <adiel.gomez020601@gmail.com>
2026-02-01 16:12:53 -0700

    Fix windows script for better output

--- tarea1_diagnostico.ps1
+++ tarea1_diagnostico.ps1
@@ -6,7 +6,7 @@ $hostname = $env:COMPUTERNAME
 Write-Host "Host: $hostname"
 Write-Host ""
 Write-Host "IP's : "
-Get-NetIPAddress -AddressFamily IPv4 | Select-Object InterfaceAlias, IPAddress
+Get-NetIPAddress -AddressFamily IPv4 | Select-Object InterfaceAlias, IPAddress | Format-Table -AutoSize | Out-String
 Write-Host ""
 Write-Host "Espacio en el disco:"
-Get-Volume
+Get-Volume | Format-Table -AutoSize | Out-String
