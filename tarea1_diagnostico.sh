be1e56a2c454492f1d05f5cbba02d5005a286c01
Suspect <50780739+suspecttaco@users.noreply.github.com>
2026-02-01 21:40:29 -0700

    Fix awk command
    Output break

--- tarea1_diagnostico.sh
+++ tarea1_diagnostico.sh
@@ -4,7 +4,7 @@
 echo "Hostname: $(hostname)"
 
 echo -e "IP's : "
-ip -4 -o addr show | awk '{print "Interfaz: " $2. "-> IP: " $4}'
+ip -4 -o addr show | awk '{print "Interfaz: " $2, "-> IP: " $4}'
 echo ""
 
 echo -e "Espacio en el disco: "
